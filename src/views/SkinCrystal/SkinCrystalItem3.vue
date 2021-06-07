f40fc9e391c2e08769761d956b04e4461992db6d
unknown <gendeulgagarin@gmail.com>
2021-06-07 13:14:01 +0800

    skincare update

--- src/views/SkinCrystal/SkinCrystalItem3.vue
+++ src/views/SkinCrystal/SkinCrystalItem3.vue
@@ -1,10 +1,5 @@
 <template>
  <ion-page>
-    <!-- <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Category 1</ion-title>
-      </ion-toolbar>
-    </ion-header> -->
 
      <ion-content :fullscreen="true" class="background">
       <ion-header collapse="condense">
@@ -20,30 +15,30 @@
             <ion-icon name="search-outline" style="color: white; font-size: 13px"></ion-icon>
             <a style="color: white; margin-left: 3px; font-size: 13px; font-family: MontserratR; letter-spacing: 1px" @click="() => router.push('/home')">MAIN MENU</a> 
             <ion-icon name="chevron-forward" style="color: white; margin-left: 3px; font-size: 13px"></ion-icon>
-            <a style="color: white; margin-left: 3px; font-size: 13px; font-family: MontserratR; letter-spacing: 1px" @click="() => router.push('/makeup')">MAKE UP</a> 
+            <a style="color: white; margin-left: 3px; font-size: 13px; font-family: MontserratR; letter-spacing: 1px" @click="() => router.push('/skincare')">SKIN CARE</a> 
             <ion-icon name="chevron-forward" style="color: white; margin-left: 3px; font-size: 13px"></ion-icon>
-            <a style="color: white; margin-left: 3px; font-size: 13px; font-family: MontserratR; letter-spacing: 1px" @click="() => router.push('/MakeupLipsList')">LIPS</a> 
+            <a style="color: white; margin-left: 3px; font-size: 13px; font-family: MontserratR; letter-spacing: 1px" @click="backToList()">CRYSTAL - FUSION DECOLLIFTE SERIES</a> 
 
             <ion-img :src="'/assets/homepage/backbutton.svg'" @click="backToList()"  style="width: 35px; float: right; right: 8px; position: absolute"/>
         </ion-list-header>
         <ion-grid>
             <ion-row style="color: white">
                 <ion-col>
-                    <!-- <ion-icon name="chevron-back" style="color: white; margin-left: 3px; font-size: 28px; margin-top: 180px; position: absolute; left: 0; z-index: 100" @click="prevSlide"></ion-icon>
-                    <ion-icon name="chevron-forward" style="color: white; margin-left: 3px; font-size: 28px; margin-top: 180px; position: absolute; right: 0; z-index: 100" @click="nextSlide"></ion-icon> -->
                     <div style="text-align: center; margin-top: 10px; color: white; z-index: -1">
-                        <p style="font-family: MontserratR;">CHUBBY LIPSTICK</p>
+                        <p style="font-family: MontserratR;">CRYSTAL - FUSION DECOLLIFTE MASK</p>
                         <ion-slides pager="false" :options="slideOpts" ref="mySlides">
                             <ion-slide>
                                 <div>
-                                    <ion-img :src="'/assets/MAKEUP CATEGORY/For lips/1.jpg'" id="product" style="margin-top: 15px; margin-bottom: 15px; height: 300px;"/>
+                                    <ion-img :src="'/assets/SKIN CARE CATEGORY/CRYSTAL - FUSION DECOLLIFTE SERIES/CRYSTAL - FUSION DECOLLIFTE MASK_40000.jpg'" id="product" style="margin-top: 15px; margin-bottom: 15px; height: 300px;"/>
                                 </div>
                             </ion-slide>
                         </ion-slides>  
-                        <p style="font-family: BodoniR;">₱5,300</p>
+                        <p style="font-family: BodoniR;">₱40,000</p>
                         <p style="font-size: 15px; font-family: MontserratR;">DETAILS</p>
-                        <p style="padding-left: 10px; padding-right: 10px; font-size: 15px; font-family: MontserratR;">This innovative Chubby Lip Pencil instantly takes any look to the next level, making your lips appear lush and plump with a charming velvety matte finish.</p>
+                        <p style="padding-left: 10px; padding-right: 10px; font-size: 15px; font-family: MontserratR;">This rejuvenating Mask, reveals a younger-looking, more even complexion of the neck and décolleté skin.</p>
                         <!-- <iframe style="margin-top: 50px" width="400" height="400" src="https://www.youtube.com/embed/dPCUT80VFm4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+                        <p style="font-size: 15px; font-family: MontserratR;">HOW TO USE:</p>
+                        <p style="padding-left: 10px; padding-right: 10px; font-size: 15px; font-family: MontserratR;">Apply an even layer onto dry and clean skin of neck and décolleté. Allow the ingredients to work for 10-15 minutes. Remove thoroughly with lukewarm water, then rinse skin with cool water (helps to shrink the pores) and pat dry. Use twice a week. For best results, follow by using CRYSTAL-FUSION DECOLLIFTE Lipid Capsules.</p>
                     </div>
                 </ion-col>
             </ion-row>
@@ -59,7 +54,7 @@ import { defineComponent, onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default defineComponent({
-    name: 'MakeupFaceItem1',
+    name: 'SkinCrystalItem2',
     components: {
         IonContent,
         IonHeader,
@@ -99,7 +94,7 @@ export default defineComponent({
 
     methods:{
         backToList() {
-            this.$router.push({name: "MakeupLipsList"})
+            this.$router.push({name: "skincrystal"})
         },
     }
     
--- src/views/SkinCrystal/SkinCrystalList.vue
+++ src/views/SkinCrystal/SkinCrystalList.vue
@@ -22,17 +22,17 @@
             <ion-grid>
                 <ion-row>
                     <ion-col style="text-align: center;">
-                        <ion-img :src="'/assets/SKIN CARE CATEGORY/CRYSTAL - FUSION DECOLLIFTE SERIES/CRYSTAL - FUSION DECOLLIFTE CREAM_15000.jpg'" @click="() => router.push('/skincheckmateItem1')"/>
+                        <ion-img :src="'/assets/SKIN CARE CATEGORY/CRYSTAL - FUSION DECOLLIFTE SERIES/CRYSTAL - FUSION DECOLLIFTE CREAM_15000.jpg'" @click="() => router.push('/skincrystalItem1')"/>
                         <ion-card-content style="padding: 9px; background-color: black; color: white; font-size: 13px; border-style: solid; border-color: grey!important; border-width: 2px;">CRYSTAL - FUSION DECOLLIFTE CREAM<br>₱15,000</ion-card-content>
                     </ion-col>
                     <ion-col style="text-align: center;">
-                        <ion-img :src="'/assets/SKIN CARE CATEGORY/CRYSTAL - FUSION DECOLLIFTE SERIES/CRYSTAL - FUSION DECOLLIFTE LIPID CAPSULES_30000.jpg'" @click="() => router.push('/skincheckmateItem2')"/>
+                        <ion-img :src="'/assets/SKIN CARE CATEGORY/CRYSTAL - FUSION DECOLLIFTE SERIES/CRYSTAL - FUSION DECOLLIFTE LIPID CAPSULES_30000.jpg'" @click="() => router.push('/skincrystalItem2')"/>
                         <ion-card-content style="padding: 9px; background-color: black; color: white; font-size: 13px; border-style: solid; border-color: grey!important; border-width: 2px;">CRYSTAL - FUSION DECOLLIFTE LIPID CAPSULES<br>₱30,000</ion-card-content>
                     </ion-col>
                 </ion-row>
                 <ion-row>
                     <ion-col style="text-align: center;">
-                        <ion-img :src="'/assets/SKIN CARE CATEGORY/CRYSTAL - FUSION DECOLLIFTE SERIES/CRYSTAL - FUSION DECOLLIFTE MASK_40000.jpg'" @click="() => router.push('/skincheckmateItem3')"/>
+                        <ion-img :src="'/assets/SKIN CARE CATEGORY/CRYSTAL - FUSION DECOLLIFTE SERIES/CRYSTAL - FUSION DECOLLIFTE MASK_40000.jpg'" @click="() => router.push('/skincrystalItem3')"/>
                         <ion-card-content style="padding: 9px; background-color: black; color: white; font-size: 13px; border-style: solid; border-color: grey!important; border-width: 2px;">CRYSTAL - FUSION DECOLLIFTE MASK<br>₱40,000</ion-card-content>
                     </ion-col> 
                     <ion-col style="text-align: center;">
